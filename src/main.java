c85029ceded906ff687b8beafe6a45756ece96f3
Chewbacca <higgimat000@warren.k12.in.us>
2024-09-18 13:29:47 -0400

    initial commit

--- src/main.java
+++ src/main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String[] args) {
+        vehicle myCar = new vehicle("Ford", "Fusion");
+        System.out.println(myCar.getMake());
+        myCar.setMake("Chevy");
+        System.out.println(myCar.getMake());
+    }
 }
--- src/vehicle.java
+++ src/vehicle.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class vehicle {
+public class vehicle {
+
+    public String make;
+    public String model;
+
+    public vehicle(String make, String model){
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
 }
